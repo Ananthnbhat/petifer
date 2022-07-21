d5c97cacd9405d7abd622bc2a156f9dce1784ed2
Ananthnbhat <ananthnbhat2@gmail.com>
2022-07-21 12:27:59 +1000

    modify model to include location & status choices

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 
 class Pet(models.Model):
-    breed = models.CharField(max_length=20)
-    color = models.CharField(max_length=20)
-    name = models.CharField(max_length=20)
-    status = models.BooleanField(default=False)
+    STATUS_TUPLE = (
+        ('lost', 'lost'),
+        ('found', 'found'),
+        ('matched', 'matched')
+    )
+    breed = models.CharField(max_length=20, default="")
+    color = models.CharField(max_length=20, default="")
+    name = models.CharField(max_length=20, default="")
+    status = models.CharField(max_length=10, choices=STATUS_TUPLE)
+    location = models.CharField(max_length=50, default="")
 
     def __str__(self) -> str:
         return self.name
