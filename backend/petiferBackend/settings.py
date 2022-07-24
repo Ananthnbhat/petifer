ed5a34873f2706bbafc3b3b3e426523b4d79cd2c
Ananthnbhat <ananthnbhat2@gmail.com>
2022-07-24 15:13:04 +1000

    add localhost to allowed hosts

--- backend/petiferBackend/settings.py
+++ backend/petiferBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-glni&wls%#spv&c5(xf@tc21@tu0!*a-h5+t8pw2@%2=-(%jit
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost']
 
 
 # Application definition
