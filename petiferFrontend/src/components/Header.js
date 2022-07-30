9f29c48c60f921637262439b6f24fe9055e9b9e3
Ananthnbhat <ananthnbhat2@gmail.com>
2022-07-30 17:52:33 +1000

    create buttons & header on Home page

--- petiferFrontend/src/components/Header.js
+++ petiferFrontend/src/components/Header.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const Header = () => {
   return (
-    <View>
-      <Text>header for all screens</Text>
+    <View style={styles.headerView}>
+      <Button
+      // icon={require('../assets/icons/petify.jpg')}
+      >
+        Image
+      </Button>
+      <Text style={styles.headerText}>Petify</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerView: {
+    height: '20%',
+    backgroundColor: '#1e4066',
+    flexDirection: 'row',
+    alignContent: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 25,
+    alignSelf: 'center',
+  }
+})
 
 export default Header;
--- petiferFrontend/src/screens/Home.js
+++ petiferFrontend/src/screens/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { fetchAllPets } from '../api';
-import { Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { RadioButton, Text, Button } from 'react-native-paper';
+
+const BUTTON_COLOR = '#bcd2e9';
 
 const Home = () => {
   useEffect(() => {
@@ -8,9 +11,114 @@ const Home = () => {
     const allPets = fetchAllPets();
     console.log(allPets);
   }, []);
+
+  const [checked, setChecked] = React.useState('lost');
+
   return (
-    <Text>Home page</Text>
+    <>
+      <View style={styles.selectOption}>
+        <TouchableOpacity
+          onPress={() => setChecked('lost')}
+          style={[styles.radioWrapper, checked === 'lost' ? styles.bgColor : null]}>
+          <View>
+            <RadioButton
+              value='lost'
+              status={checked === 'lost' ? 'checked' : 'unchecked'}
+              onPress={() => setChecked('lost')}
+              color='black'
+            />
+          </View>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.label}>Lost pet</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setChecked('found')}
+          style={[styles.radioWrapper, checked === 'found' ? styles.bgColor : null]}>
+          <View>
+            <RadioButton
+              value='found'
+              status={checked === 'found' ? 'checked' : 'unchecked'}
+              onPress={() => setChecked('found')}
+              color='black'
+            />
+          </View>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.label}>Found pet</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View>
+        {checked === 'lost' ?
+          <Button
+            icon="upload"
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+            color={BUTTON_COLOR}
+            style={styles.uploadBtn}
+            labelStyle={{ fontSize: 30 }}
+          >
+            Upload
+          </Button>
+          :
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+            color={BUTTON_COLOR}
+            style={styles.uploadBtn}
+            labelStyle={{ fontSize: 30 }}
+          >
+            Take a photo
+          </Button>
+        }
+
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  selectOption: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 40,
+  },
+  radioWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'center',
+    width: '35%',
+    paddingVertical: 8,
+    borderRadius: 7,
+
+  },
+  labelWrapper: {
+    alignSelf: 'center',
+  },
+  label: {
+    fontSize: 18
+  },
+  bgColor: {
+    backgroundColor: BUTTON_COLOR,
+    borderColor: BUTTON_COLOR,
+    borderWidth: 1,
+    // for android
+    elevation: 5,
+    // for ios
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  uploadBtn: {
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+    borderRadius: 40,
+  },
+})
 
 export default Home;
