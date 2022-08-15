afd472022fa86a658705b4d51b2e888b2a2c5500
Ananthnbhat <ananthnbhat2@gmail.com>
2022-08-15 10:16:27 +1000

    create mock json file for showing matched pets

--- petiferFrontend/src/screens/MatchedPets.js
+++ petiferFrontend/src/screens/MatchedPets.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import matchedPets from '../assets/test/matched-pets.json'
+
 const MatchedPets = () => {
   return <Text>Open this page to show matched pets</Text>;
 };
